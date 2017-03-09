e872fb17e2be71c8e45000c07d2255c761e69daf
Alexis Tyler <xo@wvvw.me>
2017-03-09 14:12:28 +1030

    fixup package file, add LICENSE, babel, yarn.lock

--- meta.js
+++ meta.js
@@ -14,21 +14,37 @@ module.exports = {
         author: {
             type: "string",
             label: "Author"
+        },
+        needClient: {
+            type: "boolean",
+            label: "Need a client?"
+        },
+        needServer: {
+            type: "boolean",
+            label: "Need a server?"
         }
     },
-    complete(data) {
+    "filters": {
+        "client/**/*": "needClient",
+        "server/**/*": "needServer"
+    },
+    complete(data, {helpers, logger}){
         if (!data.inPlace) {
-            const exec = require('child_process').exec;
-            exec(`cd ${data.destDirName} && yarn install`, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-                console.log(`stderr: ${stderr}`);
-            }).then(() => {
-                console.log(`To get started:\n\n  cd ${data.destDirName}\n  yarn start`);
-            });
+            if (!data.needClient && !data.needServer) {
+                logger.log('Not generating any files as you didn\'t want a client or server.');
+            } else {
+                const exec = require('child_process').exec;
+                clientGit = `${data.needClient ? `cd ${data.destDirName}/client && git init && cd ../..` : ''}`;
+                serverGit = `${data.needServer ? `cd ${data.destDirName}/server && git init && cd ../..` : ''}`;
+                exec(`${clientGit} && ${serverGit}`, (error, stdout, stderr) => {
+                exec('echo $(pwd)', (error, stdout, stderr) => {
+                    if (error) {
+                        logger.log(`exec error: ${error}`);
+                        return;
+                    }
+                });
+                logger.log(`To get started:\n\n  cd ${data.destDirName}\n  yarn install\n  yarn start-dev`);
+            }
         }
     },
     helpers: {
--- template/client/src/services.js
+++ template/client/src/services.js
@@ -5,7 +5,7 @@ import socketio from 'feathers-socketio/client';
 import authentication from 'feathers-authentication-client';
 import {signin} from './store/actions';
 
-const socket = io('http://bella.tardis:3030/');
+const socket = io('http://localhost:3030/');
 export const app = feathers().configure(socketio(socket)).configure(hooks()).configure(authentication({
     storage: window.localStorage,
     path: '/authentication',
